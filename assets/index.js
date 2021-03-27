e9ae15cacaf0d045c5592bf3a6ad08da2eb626b2
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-03-27 17:28:16 -0400

    started doodler logic

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,12 @@
 //this loads the page and doesn't allow an automatic refresh
 document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid');
+	const doodler = document.createElement('div');
+
+	// creates the doodler. Appends it to the grid.
+	function createDoodler() {
+		grid.appendChild(doodler);
+		doodler.classList.add('doodler');
+	}
+	createDoodler();
 });
