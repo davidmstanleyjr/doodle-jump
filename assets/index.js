c647625578f63f68945b1a148487eda1707026b9
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-03-27 18:50:50 -0400

    wrote some movement logic and the start game logic

--- assets/index.js
+++ assets/index.js
@@ -2,11 +2,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid');
 	const doodler = document.createElement('div');
+	let doodlerLeftSpace = 50;
+	let doodlerBottomSpace = 150;
+	let isGameOver = false;
 
 	// creates the doodler. Appends it to the grid.
 	function createDoodler() {
 		grid.appendChild(doodler);
 		doodler.classList.add('doodler');
+		doodler.style.left = doodlerLeftSpace + 'px';
+		doodler.style.bottom = doodlerBottomSpace + 'px';
 	}
-	createDoodler();
+
+	//starts the doodler game. Doodler appears if this function is invoked.
+	function start() {
+		if (!isGameOver) {
+			createDoodler();
+		}
+	}
 });
