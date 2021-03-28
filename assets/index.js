014339684e47f7329bcb30507d5ef77d37bbca5f
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-03-27 20:57:40 -0400

    created platforms

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	let doodlerLeftSpace = 50;
 	let doodlerBottomSpace = 150;
 	let isGameOver = false;
+	let platformCount = 5;
 
 	// creates the doodler. Appends it to the grid.
 	function createDoodler() {
@@ -14,10 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
 		doodler.style.bottom = doodlerBottomSpace + 'px';
 	}
 
+	class Platform {
+		constructor(newPlatformBottom) {
+			this.bottom = newPlatformBottom;
+			this.left = Math.random() * 315;
+			this.visual = document.createElement('div');
+
+			const visual = this.visual;
+			visual.classList.add('platform');
+			visual.style.left = this.left + 'px';
+			visual.style.bottom = this.bottom + 'px';
+			grid.appendChild(visual);
+		}
+	}
+	//this creates platforms
+	function createPlatforms() {
+		for (let i = 0; i < platformCount; i++) {
+			let platFormGap = 600 / platformCount;
+			let newPlatformBottom = 100 + i * platFormGap;
+			let newPlatForm = newPlatForm();
+		}
+	}
+
 	//starts the doodler game. Doodler appears if this function is invoked.
 	function start() {
 		if (!isGameOver) {
 			createDoodler();
+			createPlatforms();
 		}
 	}
+
+	start();
 });
